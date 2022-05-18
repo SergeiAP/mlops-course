5e4b3d1a271b5647066d7a190b7ed0cba3501706
Sergei Parshin <Sergei.A.P@yandex.com>
2022-05-18 23:24:44 +0300

    TEST DVC metrics show

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,6 +1,7 @@
 import click
 import pandas as pd
 import joblib as jb
+import json
 from typing import List
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -11,6 +12,12 @@ import lightgbm as lgb
 @click.argument("input_paths", type=click.Path(exists=True), nargs=2)
 @click.argument("output_path", type=click.Path())
 def evaluate(input_paths: List[str], output_path: str):
+    """_summary_
+
+    Args:
+        input_paths (List[str]): _description_
+        output_path (str): _description_
+    """
     test_df = pd.read_csv(input_paths[0])
     model = jb.load(input_paths[1])
 
@@ -18,15 +25,13 @@ def evaluate(input_paths: List[str], output_path: str):
     y_holdout = test_df['price']
 
     y_predicted = model.predict(x_holdout, num_iteration=model.best_iteration)
-    score = pd.DataFrame(
-        dict(
-            mae=mean_absolute_error(y_holdout, y_predicted),
-            rmse=mean_squared_error(y_holdout, y_predicted)
-        ),
-        index=[0],
-    )
-
-    score.to_csv(output_path, index=False)
+    score = dict(
+        mae=mean_absolute_error(y_holdout, y_predicted),
+        rmse=mean_squared_error(y_holdout, y_predicted)
+        )
+    
+    with open(output_path, "w") as score_file:
+        json.dump(score, score_file, indent=4)
 
 
 
